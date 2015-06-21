a4347ea17535506ee9067eaf130f55bc8dba742c
Josh Kosmala <josh@novaweb.co.nz>
2015-06-22 11:46:15 +1200

    Constructor

--- code/NZRegionDropdownField.php
+++ code/NZRegionDropdownField.php
@@ -22,4 +22,23 @@ class NZRegionDropdownField extends DropdownField {
     'International' => 'International'
   );
 
+  public function __construct($name, $title=null, $source, $value='', $form=null, $emptyString=null) {
+		$this->setSource($source);
+
+		if($emptyString === true) {
+			Deprecation::notice('3.1',
+				'Please use setHasEmptyDefault(true) instead of passing a boolean true $emptyString argument',
+				Deprecation::SCOPE_GLOBAL);
+		}
+		if(is_string($emptyString)) {
+			Deprecation::notice('3.1', 'Please use setEmptyString() instead of passing a string emptyString argument.',
+				Deprecation::SCOPE_GLOBAL);
+		}
+
+		if($emptyString) $this->setHasEmptyDefault(true);
+		if(is_string($emptyString)) $this->setEmptyString($emptyString);
+
+		parent::__construct($name, ($title===null) ? $name : $title, $value, $form);
+	}
+
 }
