f9547465339e83b81bb543be0a2ec129fef9601b
Josh Kosmala <josh@novaweb.co.nz>
2015-06-22 14:44:32 +1200

    Region dropdown field working, deleted config as source

--- code/NZRegionDropdownField.php
+++ code/NZRegionDropdownField.php
@@ -1,15 +1,32 @@
 <?php
 
-class NZRegionDropdownField extends DropdownField {
+class NZRegionDropdownField extends DropdownField
+{
+    private static $regions = array('Northland' => 'Northland',
+    'Auckland' => 'Auckland',
+    'Waikato' => 'Waikato',
+    'Bay of Plenty' => 'Bay of Plenty',
+    'Gisborne' => 'Gisborne',
+    'Hawke’s Bay' => 'Hawke’s Bay',
+    'Taranaki' => 'Taranaki',
+    'Manawatu-Wanganui' => 'Manawatu-Wanganui',
+    'Wellington' => 'Wellington',
+    'Nelson' => 'Nelson',
+    'Marlborough' => 'Marlborough',
+    'West Coast' => 'West Coast',
+    'Canterbury' => 'Canterbury',
+    'Otago' => 'Otago',
+    'Southland' => 'Southland',
+    'Chatham Islands' => 'Chatham Islands',
+    'International' => 'International', ); // = Config::inst()->get('NZRegionDropdownField', 'Regions');
 
-  private static $regions = Config::inst()->get('NZRegionDropdownField', 'Regions');
 
-
-  public function __construct($name, $title=null, $source, $value='', $form=null, $emptyString=null) {
-		// Get the regions from config.yml
-    $this->setSource(self::$regions);
-
-		parent::__construct($name, ($title===null) ? $name : $title, $value, $form);
-	}
-
+  public function __construct($name, $title = null, $source = null, $value = '', $form = null, $emptyString = null)
+  {
+      // Get the regions from config.yml
+   parent::__construct($name, ($title === null) ? $name : $title, $value, $form);
+      if (!$source) {
+          $this->setSource(self::$regions);
+      }
+  }
 }
