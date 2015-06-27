cac44d7e164ff745762af566f06df5015e2d37a6
Peter Thaleikis <spekulatius@users.noreply.github.com>
2015-06-27 16:22:30 +1200

    some code style stuff

--- code/NZRegionDropdownField.php
+++ code/NZRegionDropdownField.php
@@ -1,32 +1,44 @@
 <?php
 
-class NZRegionDropdownField extends DropdownField
-{
-    private static $regions = array('Northland' => 'Northland',
-    'Auckland' => 'Auckland',
-    'Waikato' => 'Waikato',
-    'Bay of Plenty' => 'Bay of Plenty',
-    'Gisborne' => 'Gisborne',
-    'Hawke’s Bay' => 'Hawke’s Bay',
-    'Taranaki' => 'Taranaki',
-    'Manawatu-Wanganui' => 'Manawatu-Wanganui',
-    'Wellington' => 'Wellington',
-    'Nelson' => 'Nelson',
-    'Marlborough' => 'Marlborough',
-    'West Coast' => 'West Coast',
-    'Canterbury' => 'Canterbury',
-    'Otago' => 'Otago',
-    'Southland' => 'Southland',
-    'Chatham Islands' => 'Chatham Islands',
-    'International' => 'International', ); // = Config::inst()->get('NZRegionDropdownField', 'Regions');
+class NZRegionDropdownField extends DropdownField {
+	/**
+	 * @var array
+	 */
+	private static $regions = array(
+		'Northland' => 'Northland',
+		'Auckland' => 'Auckland',
+		'Waikato' => 'Waikato',
+		'Bay of Plenty' => 'Bay of Plenty',
+		'Gisborne' => 'Gisborne',
+		'Hawke’s Bay' => 'Hawke’s Bay',
+		'Taranaki' => 'Taranaki',
+		'Manawatu-Wanganui' => 'Manawatu-Wanganui',
+		'Wellington' => 'Wellington',
+		'Nelson' => 'Nelson',
+		'Marlborough' => 'Marlborough',
+		'West Coast' => 'West Coast',
+		'Canterbury' => 'Canterbury',
+		'Otago' => 'Otago',
+		'Southland' => 'Southland',
+		'Chatham Islands' => 'Chatham Islands',
+		'International' => 'International',
+	); // = Config::inst()->get('NZRegionDropdownField', 'Regions');
 
+	/**
+	 * @param string $name
+	 * @param string $title
+	 * @param DataList $source
+	 * @param string $value
+	 * @param Form $form
+	 * @param string $emptyString
+	 */
+	public function __construct($name, $title = null, $source = null, $value = '', $form = null, $emptyString = null)
+	{
+		// Get the regions from config.yml
+		parent::__construct($name, ($title === null) ? $name : $title, $value, $form);
 
-  public function __construct($name, $title = null, $source = null, $value = '', $form = null, $emptyString = null)
-  {
-      // Get the regions from config.yml
-   parent::__construct($name, ($title === null) ? $name : $title, $value, $form);
-      if (!$source) {
-          $this->setSource(self::$regions);
-      }
-  }
+		if (!$source) {
+			$this->setSource(self::$regions);
+		}
+	}
 }
